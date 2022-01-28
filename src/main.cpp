47c8bef64035517de5b68088f235a2abfd8c4f8d
Trần Đức Trung <38934471+niits@users.noreply.github.com>
2022-01-28 23:45:01 +0700

    feat: add model loading and inference

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <torch/script.h> // One-stop header.
+
+#include <iostream>
+#include <memory>
+
+int main(int argc, const char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+        return -1;
+    }
+
+    torch::jit::script::Module module;
+    torch::NoGradGuard no_grad;
+    try
+    {
+        module = torch::jit::load(argv[1]);
+        std::vector<torch::jit::IValue> inputs;
+        inputs.push_back(torch::ones({1, 3, 224, 224}));
+
+        at::Tensor output = module.forward(inputs).toTensor();
+        std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+    }
+    catch (const c10::Error &e)
+    {
+        std::cerr << "error loading the model\n";
+        return -1;
+    }
+
+    std::cout << "ok\n";
+}
